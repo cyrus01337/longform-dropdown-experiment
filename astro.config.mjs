3a8b014d350a378c6d8c1840ff074b46d7fe4fef
cyrus01337 <cyrus21337@gmail.com>
2024-10-14 07:01:16 +0000

    Remove default build settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    base: "astro-app-boilerplate",
     integrations: [tailwind()],
     server: {
         port: 3000,
         host: true,
     },
-    site: "https://cyrus01337.github.io",
 });
